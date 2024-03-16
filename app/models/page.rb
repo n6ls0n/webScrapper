09500397a69feea737bacfc44f1b4722c036df39
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-03-15 21:10:37 -0400

    Added validation code and "run_check!" function that sets value of the results object

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,19 @@
 class Page < ApplicationRecord
+  validates :url, presence: true
+  validates :check_type, presence: true
+  validates :selector, presence: true
+  validates :match_text, presence: true, if: -> { check_type == 'text' }
+
+  def run_check!
+    scraper = Scraper.new(url)
+    result =  case check_type
+              when 'text'
+                scraper.text(selector: selector).downcase == match_text.downcase
+              when 'present'
+                scraper.present?
+              when 'not_present'
+                !scraper.present?
+              end
+    results.create(success: result)
+  end
 end
