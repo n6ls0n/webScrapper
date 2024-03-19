30ee6107bf44ab274b5b2dfa35b6bf18d4024dea
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-03-18 21:01:50 -0400

    Updated schema to account for the new "name" column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_15_203014) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_19_005602) do
   create_table "pages", force: :cascade do |t|
     t.string "url"
     t.string "check_type"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_15_203014) do
     t.string "match_text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "results", force: :cascade do |t|
